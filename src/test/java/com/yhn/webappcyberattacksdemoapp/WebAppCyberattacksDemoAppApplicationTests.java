6f0bb9c6a147a61b0e33343e8a310681edf60766
Yalchan <projectsyhn@gmail.com>
2022-12-25 13:24:05 +0200

    Create initial project

---
+++ src/test/java/com/yhn/webappcyberattacksdemoapp/WebAppCyberattacksDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhn.webappcyberattacksdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppCyberattacksDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
