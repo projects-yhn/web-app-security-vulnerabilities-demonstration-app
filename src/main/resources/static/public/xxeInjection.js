e20f60de2c66516aac964283ee9264a9db9be0ad
projects-yhn <projectsyhn@gmail.com>
2023-06-28 22:49:51 +0300

    Add Css style to XEE atack main screen

--- src/main/resources/static/public/xxeInjection.js
+++ src/main/resources/static/public/xxeInjection.js
@@ -13,28 +13,60 @@ const  getMyProducts = () => {
 
 const displayProductsAsList = (products, container) => {
     container.innerHTML = "";
-    const productsContainer = document.createElement("div");
-    for (const product of products) {
-        const productContainer = document.createElement("div");
+    const pageHeader = document.createElement('h1');
+    pageHeader.innerText = 'My products for selling';
+    pageHeader.className = 'pageHeader';
 
-        productContainer.className = "product";
-        for (const productKey in product) {
-            const productDataSpan = document.createElement("span");
-            productDataSpan.textContent = product[productKey];
-            productDataSpan.className = "productData";
-            productContainer.appendChild(productDataSpan);
-        }
+    const productsContainer = document.createElement("div");
+    productsContainer.className = 'products';
+    for (const product of products) {
+        const productContainer = prepareProductContainer(product);
         const id = product["id"];
 
-        productContainer.addEventListener('click', () =>
-        {
+        productContainer.addEventListener('click', () => {
             window.location = `${productPath}?productId=${id}`;
         });
 
         productsContainer.appendChild(productContainer);
     }
+    container.appendChild(pageHeader);
     container.appendChild(productsContainer);
+}
 
+const prepareProductContainer = product => {
+    const productContainer = document.createElement("div");
+    productContainer.className = 'product';
+
+    const imageContainer = document.createElement("img");
+    imageContainer.className = 'productImage'
+    imageContainer.src = product.imageUrl;
+
+    const productText = document.createElement('div');
+    productText.className = 'productText';
+
+    const productTitle = document.createElement('h1');
+    productTitle.className = 'productTitle';
+    productTitle.innerText = product.name;
+    productText.appendChild(productTitle);
+
+    const productCategory = document.createElement('h5');
+    productCategory.className = 'productCategory';
+    productCategory.innerText = product.category;
+    productText.appendChild(productCategory);
+
+    const productDescription = document.createElement('h4');
+    productDescription.className = 'productDescription';
+    productDescription.innerText = product.description;
+    productText.appendChild(productDescription);
+
+    const productPrice = document.createElement('h2');
+    productPrice.className = 'productPrice';
+    productPrice.innerText = `${product.price}$`;
+    productText.appendChild(productPrice);
+
+    productContainer.appendChild(imageContainer);
+    productContainer.appendChild(productText);
+    return productContainer;
 }
 
 getMyProducts()
